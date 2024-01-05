f5718e0efbbec8d5573798c407fbdc8419412746
inner-outer-space <lindasteinberger@gmail.com>
2024-01-05 18:05:15 +0100

    week 1 more changes

--- week_1_basics_n_setup/docker_sql/ingest-data.py
+++ week_1_basics_n_setup/docker_sql/ingest-data.py
@@ -11,21 +11,27 @@ def main(params):
     host = params.host
     port = params.port
     db = params.db
-    table_name = params.table_name
-    url = params.url
-    parquet_name = 'output.parquet'
+    data_table_name = params.data_table_name
+    lookup_table_name = params.lookup_table_name
+    data_url = params.data_url
+    lookup_url = params.lookup_url
+    parquet_name = 'data_output.parquet'
+    csv_name = 'lookup_output.csv'
     
     # DOWNLOAD THE DATA
-    os.system(f'wget -O {parquet_name} {url}')
+    os.system(f'wget -O {parquet_name} {data_url}')
+    os.system(f'wget -O {csv_name} {lookup_url}')
     
     # CREATE A CONNECTION TO THE DB
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
     # READ THE PARQUET and CSV FILES INTO DATAFRAMES
     df = pd.read_parquet(parquet_name)
+    df_zones = pd.read_csv(csv_name)
 
     # UPLOAD THE DATA TO THE DB
-    df.to_sql(name=table_name, con=engine, if_exists='replace')
+    df.to_sql(name=data_table_name, con=engine, if_exists='replace')
+    df_zones.to_sql(name=lookup_table_name, con=engine, if_exists='replace')
 
 
 # The parser is used to parse the command line arguments which are then passed to the main method. 
@@ -37,10 +43,10 @@ if __name__ == '__main__':
     parser.add_argument('--host', help='host for postgres')
     parser.add_argument('--port', help='port for postgres')
     parser.add_argument('--db', help='database name')
-    parser.add_argument('--table_name', help='table name')
-    parser.add_argument('--url', help='url for data file')
+    parser.add_argument('--data_table_name', help='data table name')
+    parser.add_argument('--lookup_table_name', help='zones lookup table name')
+    parser.add_argument('--data_url', help='url for data Parquet file')
+    parser.add_argument('--lookup_url', help='url for zones lookup CSV file')
 
     args = parser.parse_args()
     main(args)
-
-
