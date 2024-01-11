276d5463e0d7cc5b6d368c442b92523db586d406
inner-outer-space <lindasteinberger@gmail.com>
2024-01-11 18:54:43 +0100

    working on cleaning

---
+++ week_1_basics_n_setup/docker_sql/mappings.py
@@ -0,0 +1,35 @@
+# DEFINE MAPPING DICTIONARY
+# For analysis purposes the 'store and forward flag' will be converted from Y/N to 1/2  
+# a mapping is added here to convert back 
+
+mapping_dict = {
+    "vendor_id": {
+        1: "Creative Mobile Technologies",
+        2: "VeriFone"
+    },
+    "rate_code": {
+        1: "Standard rate",
+        2: "JFK",
+        3: "Newark",
+        4: "Nassau or Westchester",
+        5: "Negotiated fare",
+        6: "Group ride"
+    },
+    "store_and_fwd_flag": {
+        True: "Yes",
+        False: "No"
+    },
+    "payment_type": {
+        1: "Credit card",
+        2: "Cash",
+        3: "No charge",
+        4: "Dispute",
+        5: "Unknown",
+        6: "Voided trip"
+    }
+}
+
+# Example usage:
+# print(mapping_dict["vendor_id"].get(1, "Unknown"))
+# print(mapping_dict["vendor_id"][2])
+# print(mapping_dict["payment_type"][3])  
