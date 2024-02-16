7afb0442596d88f2b9eb7944e2bfbfef0006c8e7
inner-outer-space <lindasteinberger@gmail.com>
2024-02-16 15:46:30 +0100

    week 5 work

---
+++ 5-batch/test.py
@@ -0,0 +1,9 @@
+types.StructType(
+    types.StructField('hvfhs_license_num', types.StringType(), True),
+    types.StructField('dispatching_base_num', types.StringType(), True), 
+    types.StructField('pickup_datetime', types.TimestampType(), True), 
+    types.StructField('dropoff_datetime',types.TimestampType(), True), 
+    types.StructField('PULocationID', types.IntegerType(), True), 
+    types.StructField('DOLocationID', types.IntegerType(), True), 
+    types.StructField('SR_Flag', types.StringType(), True)
+)
