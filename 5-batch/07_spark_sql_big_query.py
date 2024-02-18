b4d15c3b223189d716b14d6d3f6b7cc4778f1eef
inner-outer-space <lindasteinberger@gmail.com>
2024-02-18 18:49:42 +0100

    more week 5

--- 5-batch/07_spark_sql_big_query.py
+++ 5-batch/07_spark_sql_big_query.py
@@ -1,27 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-# EXAMPLE ON LOCAL 
-# spark-submit \
-#     --master spark://pepper:7077 \
-#     07_spark_sql.py \
-#     --input_green=data/pq/green/2021/*/ \
-#     --input_yellow=data/pq/yellow/2021/*/ \
-#     --output=data/pq/output/report-2021
-
-# #
-# EXAMPLE FROM CLI DATAPROC 
-gcloud dataproc jobs submit pyspark \
-    --cluster=de-datatalks \
-    --region=europe-west6 \
-    gs://ny-taxi-data-for-spark/code/07_spark_sql.py \
-    -- \
-        --input_green=gs://ny-taxi-data-for-spark/pq/green/2020/*/ \
-        --input_yellow=gs://ny-taxi-data-for-spark/pq/yellow/2020/*/ \
-        --output=gs://ny-taxi-data-for-spark/pq/output/report-2020
-
-
 import argparse
 
 import pyspark
@@ -41,10 +20,12 @@ input_green = args.input_green
 input_yellow = args.input_yellow
 output = args.output
 
-# do not hard code the master. Define in spark-submit instead
+
 spark = SparkSession.builder \
     .appName('test') \
     .getOrCreate()
+
+spark.conf.set('temporaryGcsBucket', 'dataproc-temp-europe-west6-828225226997-fckhkym8')
 
 df_green = spark.read.parquet(input_green)
 
@@ -60,7 +41,7 @@ df_yellow = df_yellow \
     .withColumnRenamed('tpep_dropoff_datetime', 'dropoff_datetime')
 
 
-common_columns = [
+common_colums = [
     'VendorID',
     'pickup_datetime',
     'dropoff_datetime',
@@ -84,11 +65,11 @@ common_columns = [
 
 
 df_green_sel = df_green \
-    .select(common_columns) \
+    .select(common_colums) \
     .withColumn('service_type', F.lit('green'))
 
 df_yellow_sel = df_yellow \
-    .select(common_columns) \
+    .select(common_colums) \
     .withColumn('service_type', F.lit('yellow'))
 
 
@@ -99,7 +80,7 @@ df_trips_data.registerTempTable('trips_data')
 
 df_result = spark.sql("""
 SELECT 
-    -- Revenue grouping 
+    -- Reveneue grouping 
     PULocationID AS revenue_zone,
     date_trunc('month', pickup_datetime) AS revenue_month, 
     service_type, 
@@ -115,8 +96,8 @@ SELECT
     SUM(congestion_surcharge) AS revenue_monthly_congestion_surcharge,
 
     -- Additional calculations
-    AVG(passenger_count) AS avg_monthly_passenger_count,
-    AVG(trip_distance) AS avg_monthly_trip_distance
+    AVG(passenger_count) AS avg_montly_passenger_count,
+    AVG(trip_distance) AS avg_montly_trip_distance
 FROM
     trips_data
 GROUP BY
@@ -124,9 +105,10 @@ GROUP BY
 """)
 
 
-df_result.coalesce(1) \
-    .write.parquet(output, mode='overwrite')
-
+df_result.write.format('bigquery') \
+    .option('table', output) \
+    .save()
+    
 
 
 
